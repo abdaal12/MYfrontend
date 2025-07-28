5ab96b41c8e9e88cd9b1cd96abfe2983e94ac2be
Abdaal Ali <136990423+abdaal12@users.noreply.github.com>
2025-07-28 20:18:11 +0530

    Update Dashboard.jsx
    New 7odate

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,18 +2,26 @@ import React, { useState } from "react";
 import MyProducts from "../components/MyProducts";
 import AddProduct from "../components/AddProduct";
 
-
-
 const Dashboard = () => {
   const [section, setSection] = useState("products");
 
   return (
     <div className="container mt-4">
-      <h2 className="text-center mb-3">Your Dashboard>
+      <h2 className="text-center mb-3">Your Dashboard</h2>
 
       <div className="d-flex justify-content-center gap-2 mb-4 flex-wrap">
-        <button className="btn btn-outline-primary" onClick={() => setSection("products")}>My Products</button>
-        <button className="btn btn-outline-success" onClick={() => setSection("add")}>Add Product</button>
+        <button
+          className="btn btn-outline-primary"
+          onClick={() => setSection("products")}
+        >
+          My Products
+        </button>
+        <button
+          className="btn btn-outline-success"
+          onClick={() => setSection("add")}
+        >
+          Add Product
+        </button>
       </div>
 
       {section === "products" && <MyProducts />}
